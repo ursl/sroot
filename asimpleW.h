04af21b7be9922397162ca69a498b4190c0edff9
Urs Langenegger <urslangenegger@gmail.com>
2020-05-07 15:49:28 +0200

    Update asimpleW.h

--- asimpleW.h
+++ asimpleW.h
@@ -1,11 +1,6 @@
 //#import "asimple.hh"
 #include <Foundation/Foundation.h>
 
-// clang++ -c -o asimple.o  asimple.cc
-// clang++ -c -o asimpleW.o asimpleW.mm
-// swiftc -c  -o main.o -import-objc-header asimpleW.h -framework Foundation -I/usr/include main.swift
-// swiftc -o main -framework Foundation -lc++ main.o asimpleW.o asimple.o
-
 @interface asimpleW: NSObject
 
 - (void)printMe;
